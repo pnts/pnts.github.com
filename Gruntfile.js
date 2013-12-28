a8013c4e5b54f73eb0c2c0d1789085ae8b9a9a23
pnts <hi@pnts.us>
2013-12-27 20:47:41 -0500

    Enough with grunt for today!

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,14 +22,40 @@ module.exports = function(grunt) {
             dest: 'assets/img/build'
         	}]
     		}
-			}
+			},
+
+			watch: {
+				options: {
+        	livereload: true,
+    		},
+    		
+    		scripts: {
+        	files: ['js/*.js'],
+        	tasks: ['concat', 'uglify'],
+        	options: {
+            spawn: false,
+        	}
+    		},
+    		
+    		css: {
+        	files: ['assets/css/*.scss'],
+        	tasks: ['sass'],
+        	options: {
+          	spawn: false,
+        	}
+      	}, 
+			},
 		});
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['concat', 'imagemin']);
+
+    grunt.registerTask('dev', ['watch']);
 
   };
