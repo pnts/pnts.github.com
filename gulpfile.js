e8ce5874ccc0b46d0bdcd06f686f19452288c875
pnts <hi@pnts.us>
2014-06-21 20:19:00 -0400

    gulp should watch sass files

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,19 @@ var sass = require('gulp-ruby-sass');
 
 
 gulp.task('default', function() {
+
+});
+
+gulp.task('sass', function() {
   return gulp.src('.assets/scss/*.scss')
     .pipe(sass({sourcemap: true}))
     .pipe(gulp.dest('.assets/css'));
 });
 
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch('./assets/scss/*.scss', ['sass']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch', 'sass']);
