a2bd399934b5c34b1c68a57fcf75926f4465dae1
pnts <hi@pnts.us>
2016-01-21 22:33:18 -0500

    simple gulp for sass and autoprefixing

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     notify = require('gulp-notify'),
     livereload = require('gulp-livereload'),
@@ -7,30 +7,13 @@ var gulp = require('gulp'),
 
 
 gulp.task('styles', function() {
-  return sass('assets/scss/*.scss')
-  .pipe(autoprefixer('last 2 version'))
-  .pipe(gulp.dest('assets/css'))
-  .pipe(notify({ message: 'styles are done baking'}));
+  gulp.src('./assets/scss/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer('last 2 version'))
+      .pipe(gulp.dest('./assets/css'))
+      .pipe(notify({ message: 'styles are done baking'}));
 });
 
-gulp.task('clean', function() {
-  return del(['assets/css']);
+gulp.task('default', function() {
+  gulp.watch('./assets/scss/*.scss', ['styles']);
 });
-
-gulp.task('default', ['clean'] function() {
-  gulp.start('styles');
-});
-
-gulp.task('watch', function() {
-  gulp.watch('assets/scss/*.scss');
-
-  livereload.listen();
-  gulp.watch(['_site/**']).on('change', livereload.changed);
-});
-
-gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch(['_site/**']).on('change', livereload.changed);
-});
-
-
