36ed39607e50f3f49b20355d405f130ac866829b
pnts <hi@pnts.us>
2018-06-17 17:42:59 +0200

    going back to middleman deploy

--- config.rb
+++ config.rb
@@ -46,3 +46,12 @@ configure :build do
    activate :minify_css
    activate :minify_javascript
  end
+
+ activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
